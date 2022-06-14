a045fd52e30e019e72b2c30d6d5cf5005f86e53e
Lucas da Luz <daluzdev@gmail.com>
2022-06-14 16:37:31 -0300

    Recursive error

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace StringReducer
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type a string:");
+            string input = Console.ReadLine();
+
+            try
+            {
+                string inputReduced = StringReducer.Reduce(input);
+
+                Console.Write("String inputed by user: {0}", input);
+                Console.WriteLine("String reduced: {0}", inputReduced);
+                Console.WriteLine("String reduced size: {0} character(s)", inputReduced.Length);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();            
+        }
+    }
+}
---
+++ StringReducer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace StringReducer
+{
+    public static class StringReducer
+    {
+        static bool IsInputValid(string input) =>
+            !(string.IsNullOrEmpty(input) || Regex.IsMatch(input, "[^a-c]"));
+
+        static char ReplaceEquals(char character1, char character2)
+        {
+            if (character1 == 'a' && character2 == 'b')
+                return 'c';
+            else if (character1 == 'a' && character2 == 'c')
+                return 'b';
+            else if (character1 == 'b' && character2 == 'a')
+                return 'c';
+            else if (character1 == 'b' && character2 == 'c')
+                return 'a';
+            else if (character1 == 'c' && character2 == 'a')
+                return 'b';
+            else
+                return 'a';
+        }
+
+        public static string Reduce(string input)
+        {
+            if (!IsInputValid(input))
+                throw new Exception("Text inputed is out of the norm");
+            else if (Regex.IsMatch(input, $"[a]{{{input.Length}}}|[b]{{{input.Length}}}|[c]{{{input.Length}}}"))
+                return input;
+            else if (input.Substring(0, 1) == input.Substring(1, 1))
+            {
+                string rest = Reduce(input.Substring(1));
+                return Reduce(input.Substring(0, 1) + rest);
+            }
+            else
+            {
+                char replacedCharacter = ReplaceEquals(input[0], input[1]);
+                return Reduce(replacedCharacter + input.Substring(2));
+            }
+        }
+    }
+}
